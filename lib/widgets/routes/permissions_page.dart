2ac7965c22409f7e2e326e3a6c421e6db32af3a7
BradenBagby <braden.bagby@wavv.com>
2021-04-14 19:17:34 -0500

    Android save photos

--- lib/widgets/routes/permissions_page.dart
+++ lib/widgets/routes/permissions_page.dart
@@ -13,31 +13,33 @@ class PermissionsPage extends StatelessWidget {
         }
       },
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset(
-                  "assets/images/icon_clean.png",
-                  fit: BoxFit.contain,
-                )),
-            const SizedBox(height: 16),
-            Text(
-              "Joke Cam needs access to your camera and photo library to continue!",
-              style: Theme.of(context).textTheme.subtitle1,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                BlocProvider.of<CameraBloc>(context)
-                    .add(const CameraEvent.checkPermissions(ask: true));
-              },
-              child: const Text("Got It"),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset(
+                    "assets/images/icon_clean.png",
+                    fit: BoxFit.contain,
+                  )),
+              const SizedBox(height: 16),
+              Text(
+                "Joke Cam needs access to your camera and photo library to continue!",
+                style: Theme.of(context).textTheme.subtitle1,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  BlocProvider.of<CameraBloc>(context)
+                      .add(const CameraEvent.checkPermissions(ask: true));
+                },
+                child: const Text("Got It"),
+              ),
+            ],
+          ),
         ),
       ),
     );
